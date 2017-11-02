c96ec21bca8eeaced7f9d665ace67b26869a86e4
hkd987 <lundinmatthews@me.com>
2017-11-02 14:22:48 -0500

    Minesweeper Base Code
    Base Code with the hard coded pieces

---
+++ src/minesweeper.js
@@ -0,0 +1,15 @@
+//hardcode game board parts and pieces
+const blankLine = ' | | '
+const guessLine = '1| | '
+const bombLine = ' |B| '
+
+//log message about empty game piece
+console.log('This is what an empty board would look like:');
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+//Log message about bomb game board
+console.log('This is what a board with a guess and a bomb on it would look like:');
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
